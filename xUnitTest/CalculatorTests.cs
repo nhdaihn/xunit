aeee6902e78da09776b275781b2a7f2bf8361b9e
nhdaihn <nhdaihn@gmail.com>
2020-09-13 18:30:39 +0700

    change

---
+++ xUnitTest/CalculatorTests.cs
@@ -0,0 +1,137 @@
+﻿using Service;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace xUnitTest
+{
+    public class CalculatorTests
+    {
+        // Sử dụng Fact
+        [Fact]
+        [Trait("Simple","Add")]
+        public void Add_SimpleValuesShouldCalculate()
+        {
+            var calculator = new Calculator();
+
+            int value1 = 1;
+            int value2 = 2;
+
+            var result = calculator.Add(value1, value2);
+
+            Assert.Equal(3, result);
+        }
+
+        // Ví dụ Theory vs InlineData
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(-4, -6, -10)]
+        [InlineData(-2, 2, 0)]
+        public void Add_SimpleValuesShouldCalculate1(int value1, int value2, int expected)
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Add(value1, value2);
+
+            Assert.Equal(expected, result);
+        }
+
+        // Ví dụ ClassData
+        [Theory]
+        [ClassData(typeof(CalculatorTestData))]
+        public void Add_TheoryClassData(int value1, int value2, int expected)
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Add(value1, value2);
+
+            Assert.Equal(expected, result);
+        }
+
+        // Ví dụ MemberData lấy dữ liệu từ properties
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void Add_TheoryMemberDataProperty(int value1, int value2, int expected)
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Add(value1, value2);
+
+            Assert.Equal(expected, result);
+        }
+
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+            new object[] { 1, 2, 3 },
+            new object[] { -4, -6, -10 },
+            new object[] { -2, 2, 0 },
+            };
+
+
+        // Ví dụ MemberData lấy dữ liệu từ method của test class
+        [Theory]
+        [MemberData(nameof(GetData), parameters: 3)]
+        public void Add_TheoryMemberDataMethod2(int value1, int value2, int expected)
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Add(value1, value2);
+
+            Assert.Equal(expected, result);
+        }
+
+        public static IEnumerable<object[]> GetData(int numTests)
+        {
+            var allData = new List<object[]>
+            {
+                new object[] { 1, 2, 3 },
+                new object[] { -4, -6, -10 },
+                new object[] { -2, 2, 0 },
+                new object[] { int.MinValue, -1, int.MaxValue },
+            };
+
+            return allData.Take(numTests);
+        }
+
+        // Ví dụ lấy dữ liệu từ properties của một class khác
+        [Theory]
+        [MemberData(nameof(CalculatorData.Data), MemberType = typeof(CalculatorData))]
+        public void Add_TheoryMemberDataMethod3(int value1, int value2, int expected)
+        {
+            var calculator = new Calculator();
+
+            var result = calculator.Add(value1, value2);
+
+            Assert.Equal(expected, result);
+        }
+    }
+
+    // Khởi tạo CalculatorTestData
+    public class CalculatorTestData : IEnumerable<object[]>
+    {
+        public IEnumerator<object[]> GetEnumerator()
+        {
+            yield return new object[] { 1, 2, 3 };
+            yield return new object[] { -4, -6, -10 };
+            yield return new object[] { -2, 2, 0 };
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+    }
+
+    public class CalculatorData
+    {
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+            new object[] { 1, 2, 3 },
+            new object[] { -4, -6, -10 },
+            new object[] { -2, 2, 0 },
+            };
+    }
+}
